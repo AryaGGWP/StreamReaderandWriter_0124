ced54e3162731aa604288057d7f13e0f2de93fe2
AryaGGWP <arya.lampung.2016@gmail.com>
2025-06-09 18:45:40 +0700

    membuat exception manual

--- manualException.cpp
+++ manualException.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    try 
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo");
+        cout << "IPernyataan ini tidak untuk dieksekusi" << endl;
+    }
+    catch (int a)
+    {
+        
+    }
 }
