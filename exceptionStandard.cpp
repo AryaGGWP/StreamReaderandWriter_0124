71adfc2e1eaa6d7fd76ef5986251ef1f1a909ac2
AryaGGWP <arya.lampung.2016@gmail.com>
2025-06-09 18:55:04 +0700

    membuat exception standar dengan array

---
+++ exceptionStandard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <exception>
+// menangani obyek pengecualian yang digunakan
+#include <array>
+// untuk obyek array yang digunakan
+using namespace std;
+
+int main() {
+   cout << "Awal program" << endl; // penanda 1
+   try {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array interger 3 elemen
+   }
+}
