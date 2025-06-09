b6ae129a6bd8a56b9307040c5bcc0bee4e31140e
AryaGGWP <arya.lampung.2016@gmail.com>
2025-06-09 18:31:52 +0700

    method open file secara dinamis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,28 +3,49 @@
 #include <string>
 using namespace std;
 
-int main(){
+int main() 
+{
     string baris;
-    string NamaFile;
+    string namaFile;
 
-    cout << "Masukkan nama file: ";
-    cin >> NamaFile;
+    cout << "Masukkan nama file yang akan dibuat: ";
+    cin >> namaFile;
 
-    // membuka dalam mode menulis
+    // membuat obyek output file stream dalam mode menulis
     ofstream outfile;
-    // menunjuk ke sebuah nama file
-    outfile.open(NamaFile + ".txt", ios::out);
+    // membuka file untuk menulis secara dinamis
+    outfile.open(namaFile + ".txt");
 
-    cout << "=> Menulis file, \'q\' untuk keluar" << endl;
+    cout <<  ">= Menulis file, \'q\' untuk keluar" << endl;
 
     // unlimited loop untuk menulis ke file
     while (true) {
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
-        // loop akan berhenti jika pengguna mengetik "q"
-        if (baris == "q") break;
+        // loop akan berhenti jika pengguna mengetik 'q'
+        if (baris == "q") 
+            break;
         // menulis dan memasukkan nilai dari baris ke file
-        outfile << baris << endl;
+        outfile << baris << endl;    
+    }
+    outfile.close(); // menutup file
+    
+    // membuat obyek input file stream dalam mode membaca
+    ifstream infile;
+    // membuka file untuk membaca
+    infile.open(namaFile + ".txt");
+
+    cout << endl << ">= Membuka dan Membaca file" << endl;
+    // jika file ada maka
+    if (infile.is_open()) {
+        // mmelakukan perulangan untuk membaca setiap baris
+        while (getline(infile, baris)) {
+            // menampilkan setiap baris ke layar
+            cout << baris << endl;
+        }
+        infile.close(); // menutup file
+    } else {
+        cout << "File tidak ditemukan!" << endl;
     }
 }
