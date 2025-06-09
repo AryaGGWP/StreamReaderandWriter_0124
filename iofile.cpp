2afd8b6027bc2088b77e6f4189e7e3c22bf4db3e
AryaGGWP <arya.lampung.2016@gmail.com>
2025-06-09 17:23:53 +0700

    simulasi membuat file dalam mode menulis

--- iofile.cpp
+++ iofile.cpp
@@ -3,3 +3,16 @@
 #include <string>
 using namespace std;
 
+int main() 
+{
+    string baris;
+
+    // membuat obyek output file stream dalam mode menulis
+    ofstream outfile;
+    // membuka file untuk menulis
+    outfile.open("contohfile.txt");
+
+    cout <<  ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    
+}
